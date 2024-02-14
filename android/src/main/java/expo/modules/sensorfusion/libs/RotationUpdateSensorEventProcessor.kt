800ed35ab75fcdc3ecaeb066fbf1ad5ac12ec0ad
Kishan Jadav <kishan_jadav@hotmail.com>
2024-02-14 21:52:40 +0000

    Tweak rotation matrix on android to return transposed values instead

--- android/src/main/java/expo/modules/sensorfusion/libs/RotationUpdateSensorEventProcessor.kt
+++ android/src/main/java/expo/modules/sensorfusion/libs/RotationUpdateSensorEventProcessor.kt
@@ -14,15 +14,15 @@ class RotationUpdateSensorEventProcessor() {
     val rotationMatrixBundle = bundleOf().apply {
       putBundle("rotationMatrix", bundleOf().apply {
         putDouble("m11", rotationMatrixOutput[0].toDouble())
-        putDouble("m12", rotationMatrixOutput[1].toDouble())
-        putDouble("m13", rotationMatrixOutput[2].toDouble())
+        putDouble("m21", rotationMatrixOutput[1].toDouble())
+        putDouble("m31", rotationMatrixOutput[2].toDouble())
 
-        putDouble("m21", rotationMatrixOutput[4].toDouble())
+        putDouble("m12", rotationMatrixOutput[4].toDouble())
         putDouble("m22", rotationMatrixOutput[5].toDouble())
-        putDouble("m23", rotationMatrixOutput[6].toDouble())
+        putDouble("m32", rotationMatrixOutput[6].toDouble())
 
-        putDouble("m31", rotationMatrixOutput[8].toDouble())
-        putDouble("m32", rotationMatrixOutput[9].toDouble())
+        putDouble("m13", rotationMatrixOutput[8].toDouble())
+        putDouble("m23", rotationMatrixOutput[8].toDouble())
         putDouble("m33", rotationMatrixOutput[10].toDouble())
       })
     }
